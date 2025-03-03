8bfc941827a9e3c67ab3e7a4dff8c806070b3a34
David Cocero <david_cocero@mckinsey.com>
2025-03-03 20:50:12 +0100

    Deployment

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def create_app():
 
 app = create_app()
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
